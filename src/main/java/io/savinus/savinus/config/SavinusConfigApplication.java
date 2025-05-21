4cba41dfcee3bdfce98f58be6735cbf88120fcf6
KimSejong <sjkim@ckinform.com>
2025-05-21 10:19:51 +0900

    Initial commit

---
+++ src/main/java/io/savinus/savinus/config/SavinusConfigApplication.java
@@ -0,0 +1,13 @@
+package io.savinus.savinus.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavinusConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SavinusConfigApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/savinus/savinus/config/SavinusConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package io.savinus.savinus.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavinusConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
